8004aca79a2d5e1919fbfb8389d401f87331ce0f
eddieware <47250727+eddieware@users.noreply.github.com>
2020-08-28 10:27:41 -0500

    add_mediaservice_image_picker_to_yamel

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,13 +1,21 @@
+import 'dart:io';
+
 import 'package:final_project_flutter_android/providers/provider_usuario.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-class ProfilePage extends StatelessWidget {
+class ProfilePage extends StatefulWidget {
   const ProfilePage({Key key}) : super(key: key);
 
   @override
+  _ProfilePageState createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  @override
   Widget build(BuildContext context) {
     final profileProvider = Provider.of<UsuarioInfo>(context);
+    File _image;
 
     return Scaffold(
       appBar: AppBar(title: Text('Welcome to your Profile')),
@@ -33,12 +41,23 @@ class ProfilePage extends StatelessWidget {
           padding: EdgeInsets.zero,
           children: <Widget>[
             DrawerHeader(
-              child: Text('Ejemplo de Navigation Drawer'),
+              //margin: EdgeInsets.symmetric(horizontal: 50),
+              //'https://scontent-qro1-1.xx.fbcdn.net/v/t1.0-9/89505307_2923676754359456_8811794812756819968_n.jpg?_nc_cat=111&_nc_sid=09cbfe&_nc_eui2=AeEHFe2ZkGpXFG5PiF5o63P6I5X4tzZiv-Ujlfi3NmK_5QThRhVtgEESM0J7RGSTbMnGlBn2BMR32Wfn_9bloTg0&_nc_ohc=3AVkAPkE7ckAX98PpKs&_nc_ht=scontent-qro1-1.xx&oh=ba98746c45f3140a4da07437b9cfdea1&oe=5F545C7D')
+              padding: EdgeInsets.symmetric(horizontal: 70),
+              child: Container(
+                //padding: EdgeInsets.all(8.0),
+                width: 100,
+                child: CircleAvatar(
+                  backgroundImage: NetworkImage(
+                      'https://scontent-qro1-1.xx.fbcdn.net/v/t1.0-9/89505307_2923676754359456_8811794812756819968_n.jpg?_nc_cat=111&_nc_sid=09cbfe&_nc_eui2=AeEHFe2ZkGpXFG5PiF5o63P6I5X4tzZiv-Ujlfi3NmK_5QThRhVtgEESM0J7RGSTbMnGlBn2BMR32Wfn_9bloTg0&_nc_ohc=3AVkAPkE7ckAX98PpKs&_nc_ht=scontent-qro1-1.xx&oh=ba98746c45f3140a4da07437b9cfdea1&oe=5F545C7D'),
+
+                  //radius: 30.0,
+                ),
+              ),
               decoration: BoxDecoration(
                 color: Colors.blue,
               ),
             ),
-
             ListTile(
               title: Text(
                 'Calificaciones',
@@ -48,16 +67,6 @@ class ProfilePage extends StatelessWidget {
                 Navigator.pushNamed(context, '/calif');
               },
             ),
-
-            // ListTile(
-            //   title: Text('Tarjetas'),
-            //   onTap: () {
-            //     final route2 =
-            //         MaterialPageRoute(builder: (context) => CardPage());
-
-            //     Navigator.push(context, route2);
-            //   },
-            // ),
           ],
         ),
       ),
